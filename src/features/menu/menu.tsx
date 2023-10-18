b799d5675df407179f17cdc5ae75bbdf511f0543
Peter Goldstein <peter.m.goldstein@gmail.com>
2023-10-18 13:48:44 -0400

    Add a rough tooltip component, just to get it in the code base.  Styles, etc. will require revision.

--- src/features/menu/menu.tsx
+++ src/features/menu/menu.tsx
@@ -1,10 +1,14 @@
+"use client"
+
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { BarChartHorizontalBig } from "lucide-react";
 import Link from "next/link";
+import { Tooltip, TooltipProvider } from "@/components/ui/tooltip";
 import { UserProfile } from "../user-profile";
 
 export const MainMenu = () => {
   return (
+    <TooltipProvider>
     <div className="flex gap-2 flex-col justify-between">
       <div className="flex gap-2 flex-col justify-between">
         <Link
@@ -16,6 +20,7 @@ export const MainMenu = () => {
             <AvatarImage src="/images/ai-icon.png" />
           </Avatar>
         </Link>
+        <Tooltip content="Reporting">
         <Link
           href="/reporting"
           className="w-10 h-10 items-center justify-center flex rounded-full hover:bg-secondary"
@@ -23,8 +28,10 @@ export const MainMenu = () => {
         >
           <BarChartHorizontalBig size={20} />
         </Link>
+        </Tooltip>
       </div>
       <UserProfile />
     </div>
+    </TooltipProvider>
   );
 };
