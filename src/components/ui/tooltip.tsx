fe415cba787a27ace343940a1e0df1f36e67d349
Peter Goldstein <peter.m.goldstein@gmail.com>
2023-10-18 21:37:12 -0400

    Revert remark libs to get Markdown working.  Also actually check in tooltip changes (#11)

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as TooltipPrimitives from '@radix-ui/react-tooltip';
+import { cn } from "@/lib/utils"
 
 const TooltipProvider = TooltipPrimitives.Provider
 
@@ -8,7 +9,7 @@ const Tooltip = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitives.Root> &
     React.ComponentPropsWithoutRef<typeof TooltipPrimitives.Content>
->(({ side = "right", align = "center", ...props }, ref) => {
+>(({ className, side = "right", align = "center", ...props }, ref) => {
   return (
     <TooltipPrimitives.Root
       open={props.open}
@@ -18,7 +19,11 @@ const Tooltip = React.forwardRef<
       <TooltipPrimitives.Trigger asChild>
         {props.children}
       </TooltipPrimitives.Trigger>
-      <TooltipPrimitives.Content className="TooltipContent" side={side} align={align} {...props}>
+      <TooltipPrimitives.Content className={cn(
+      "z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      className
+    )}
+        side={side} align={align} {...props}>
         {props.content}
         <TooltipPrimitives.Arrow className="TooltipArrow" width={11} height={5} />
       </TooltipPrimitives.Content>
